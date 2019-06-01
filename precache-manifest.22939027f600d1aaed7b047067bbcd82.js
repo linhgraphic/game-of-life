810440e9c0b64844532730e39315807cefc3f3c8
Linh <linhgraphic@gmail.com>
2019-06-01 17:07:44 +0100

    Updates

---
+++ precache-manifest.22939027f600d1aaed7b047067bbcd82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3717b5301ecf442df8e5a30fdb73d4a0",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "4b45b533d323da554a5f",
+    "url": "/game-of-life/static/css/main.92ce5413.chunk.css"
+  },
+  {
+    "revision": "530c17cce8ca8683144d",
+    "url": "/game-of-life/static/js/2.525bcecf.chunk.js"
+  },
+  {
+    "revision": "4b45b533d323da554a5f",
+    "url": "/game-of-life/static/js/main.70a18df3.chunk.js"
+  },
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  }
+]);
